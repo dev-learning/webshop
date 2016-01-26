cd28d552a4d69bbf21a5dbe4ef1a160dffd02733
bahtiyar <bahtiyar.kavakli@persgroep.nl>
2016-01-26 13:46:43 +0100

    product controller product service injection

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,12 +2,31 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
+use AppBundle\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProductController extends Controller
 {
-    public function createAction()
+    /**
+     * @var ProductService
+     */
+    private $productService;
+
+    /**
+     * ProductController constructor.
+     * @param ProductService $productService
+     */
+    public function __construct(ProductService $productService)
     {
-        
+        $this->productService = $productService;
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function createAction(Product $product)
+    {
+        $this->productService->store($product);
     }
 }
